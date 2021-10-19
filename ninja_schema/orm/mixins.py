cb5d9ac859c2e07f7fc9f50e496f6799c0cc4077
eadwinCode <ezeudoh.tochukwu@gmail.com>
2021-10-19 13:53:10 +0100

    fixed failing codestyle

--- ninja_schema/orm/mixins.py
+++ ninja_schema/orm/mixins.py
@@ -1,12 +1,8 @@
-from typing import TYPE_CHECKING, Callable, List, Type, Union
+from typing import Callable, Type
 
-from django.core.paginator import Page
-from django.db.models import Model as DjangoModel, QuerySet
+from django.db.models import Model as DjangoModel
 
 from ..types import DictStrAny
-
-if TYPE_CHECKING:
-    from pydantic.main import BaseModel
 
 
 class SchemaMixins:
@@ -18,13 +14,3 @@ class SchemaMixins:
         for attr, value in self.dict(**kwargs).items():
             setattr(model_instance, attr, value)
         return model_instance
-
-    @classmethod
-    def from_django(
-        cls, model_instance: Union[QuerySet, Page, DjangoModel], many: bool = False
-    ) -> Union[Type["BaseModel"], List[Type["BaseModel"]]]:
-        if many:
-            if isinstance(model_instance, (QuerySet, list, Page)):
-                return [cls.from_orm(model) for model in model_instance]  # type: ignore
-            raise Exception("model_instance must a queryset or list")
-        return cls.from_orm(model_instance)  # type: ignore
