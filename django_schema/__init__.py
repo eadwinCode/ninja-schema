f748786d600514a95d799df1f5e023a4139d840f
eadwinCode <ezeudoh.tochukwu@gmail.com>
2021-10-20 20:56:00 +0100

    0.12.1

--- django_schema/__init__.py
+++ django_schema/__init__.py
@@ -1,6 +1,6 @@
 """Django Schema - Builds Pydantic Schemas from Django Models"""
 
-__version__ = "0.10.1"
+__version__ = "0.12.1"
 
 from .orm.factory import SchemaFactory
 from .orm.model_schema import ModelSchema
