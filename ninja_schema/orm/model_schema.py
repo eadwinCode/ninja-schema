b05a81999424012f2167fa133a6d988440d37bea
eadwinCode <ezeudoh.tochukwu@gmail.com>
2021-10-25 09:55:21 +0100

    fixed mro validation issues issue on model_schema

--- ninja_schema/orm/model_schema.py
+++ ninja_schema/orm/model_schema.py
@@ -1,6 +1,8 @@
 from itertools import chain
 from typing import (
+    TYPE_CHECKING,
     Any,
+    Callable,
     Dict,
     Iterator,
     List,
@@ -14,14 +16,20 @@ from typing import (
 
 from django.db.models import Field, ManyToManyRel, ManyToOneRel
 from pydantic import BaseConfig, BaseModel, PyObject
-from pydantic.class_validators import extract_root_validators
+from pydantic.class_validators import (
+    extract_root_validators,
+    extract_validators,
+    inherit_validators,
+)
 from pydantic.fields import FieldInfo, ModelField, Undefined
 from pydantic.main import (
     ANNOTATED_FIELD_UNTOUCHED_TYPES,
+    UNTOUCHED_TYPES,
     ModelMetaclass,
+    generate_hash_function,
     validate_custom_root_type,
 )
-from pydantic.typing import get_args, get_origin, resolve_annotations
+from pydantic.typing import get_args, get_origin, is_classvar, resolve_annotations
 from pydantic.utils import (
     ROOT_KEY,
     ClassAttribute,
@@ -39,6 +47,9 @@ from .mixins import SchemaMixins
 from .model_validators import ModelValidatorGroup
 from .schema_registry import registry as global_registry
 from .utils.converter import convert_django_field_with_choices
+
+if TYPE_CHECKING:
+    from pydantic.class_validators import ValidatorListDict
 
 ALL_FIELDS = "__all__"
 
@@ -85,7 +96,11 @@ def update_class_missing_fields(
     old_namespace: PydanticNamespace = PydanticNamespace(cls)
     fields = old_namespace.__fields__ or {}
     config: Type[BaseConfig] = cast(Type[BaseConfig], old_namespace.__config__)
+    validators: "ValidatorListDict" = {}
 
+    pre_root_validators, post_root_validators = [], []
+    class_vars: Set[str] = set()
+    hash_func: Optional[Callable[[Any], int]] = None
     untouched_types = ANNOTATED_FIELD_UNTOUCHED_TYPES
 
     def is_untouched(value: Any) -> bool:
@@ -94,15 +109,24 @@ def update_class_missing_fields(
             or value.__class__.__name__ == "cython_function_or_method"
         )
 
-    vg = ModelValidatorGroup(old_namespace.__validators__)  # type: ignore
+    for base in reversed(bases):
+        if base != BaseModel:
+            validators = inherit_validators(base.__validators__, validators)  # type: ignore
+            pre_root_validators += base.__pre_root_validators__
+            post_root_validators += base.__post_root_validators__
+            class_vars.update(base.__class_vars__)
+            hash_func = base.__hash__
+
+    validators = inherit_validators(extract_validators(namespace), validators)
+    vg = ModelValidatorGroup(validators)
     new_annotations = resolve_annotations(
         namespace.get("__annotations__") or {}, getattr(cls, "__module__", None)
     )
     # annotation only fields need to come first in fields
-    class_vars = old_namespace.__class_vars__
-
     for ann_name, ann_type in new_annotations.items():
-        if is_valid_field(ann_name):
+        if is_classvar(ann_type):
+            class_vars.add(ann_name)
+        elif is_valid_field(ann_name):
             validate_field_name(bases, ann_name)
             value = namespace.get(ann_name, Undefined)
             allowed_types = (
@@ -125,6 +149,7 @@ def update_class_missing_fields(
                 config=config,
             )
 
+    untouched_types = UNTOUCHED_TYPES + config.keep_untouched
     for var_name, value in namespace.items():
         can_be_changed = var_name not in class_vars and not is_untouched(value)
         if (
@@ -155,18 +180,20 @@ def update_class_missing_fields(
     old_namespace.__annotations__.update(new_annotations)
     pre_rv_new, post_rv_new = extract_root_validators(namespace)
 
+    if hash_func is None:
+        hash_func = generate_hash_function(config.frozen)
+    pre_root_validators.extend(pre_rv_new)
+    post_root_validators.extend(post_rv_new)
+
     new_namespace = {
         "__annotations__": old_namespace.__annotations__,
         "__config__": config,
         "__fields__": fields,
         "__validators__": vg.validators,
-        "__pre_root_validators__": unique_list(
-            old_namespace.__pre_root_validators__ + pre_rv_new
-        ),
-        "__post_root_validators__": unique_list(
-            old_namespace.__post_root_validators__ + post_rv_new
-        ),
+        "__pre_root_validators__": unique_list(pre_root_validators),
+        "__post_root_validators__": unique_list(post_root_validators),
         "__class_vars__": class_vars,
+        "__hash__": hash_func,
     }
     for k, v in new_namespace.items():
         if hasattr(cls, k):
