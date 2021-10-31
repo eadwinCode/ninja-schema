10c1b8656c5afbb232f143fd08ba6dec41ad42c3
Ezeudoh Tochukwu <ezeudoh.tochukwu@gmail.com>
2021-10-31 21:49:45 +0100

    0.12.2

--- ninja_schema/__init__.py
+++ ninja_schema/__init__.py
@@ -1,6 +1,6 @@
 """Django Schema - Builds Pydantic Schemas from Django Models"""
 
-__version__ = "0.12.1"
+__version__ = "0.12.2"
 
 from .orm.factory import SchemaFactory
 from .orm.model_schema import ModelSchema
