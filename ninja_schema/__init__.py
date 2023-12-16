0df47c89db3d2bd5a6c6f4a0c4b508d6a8e7e5c2
Tochukwu <dlastdon4000@gmail.com>
2023-12-16 05:49:41 +0100

    0.13.6

--- ninja_schema/__init__.py
+++ ninja_schema/__init__.py
@@ -1,6 +1,6 @@
 """Django Schema - Builds Pydantic Schemas from Django Models with default field type validations"""
 
-__version__ = "0.13.5"
+__version__ = "0.13.6"
 
 from .orm.factory import SchemaFactory
 from .orm.model_schema import ModelSchema
